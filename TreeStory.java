d1b61c1f64005588190d00406298a7a763d8eed0
David-Mikkelson <mikkelson_d@yahoo.com>
2016-08-19 11:18:07 -0700

    Adding Hangman Game
    Hangman take 1 argument which is the word to look for.

--- TreeStory.java
+++ TreeStory.java
@@ -23,19 +23,32 @@ public class TreeStory {
        		// exit the system without errors
        		System.exit(0);
        	}
-
+        //testing contains
+        //String Big = "This is a great Sentence";
+        //if (Big.toLowerCase().contains("sentence".toLowerCase())){
+        //  console.printf("yes");
+        //}
            // __name__ is a __adjective___ __noun__.  They are always __adverb__ __verb__.
         
+        String badWords = "jerk, dork, stupid, bad";
 
         String name = console.readLine("Enter your name: ");
         String adjective = console.readLine("Enter an adjective: ");
-        String noun = console.readLine("Please enter a noun:  ");
-        //the teacher doesn't want to be called a dork or Jerk
-        if (noun.equalsIgnoreCase("dork") ||
-        	noun.equalsIgnoreCase("jerk")){
-        		console.printf("That word is not allowed. Existing\n");
-        		System.exit(0);
-        }
+        // declare the varibles that you want outside of the scope of the code block
+        String noun; boolean isInvalidWord;
+        // Example of a do while loop. It will run through once before eval
+        do {
+            noun = console.readLine("Please enter a noun:  ");
+            //the teacher doesn't want to be called a dork or Jerk
+            
+              
+            
+            isInvalidWord = (noun.equalsIgnoreCase("dork") ||
+            	               noun.equalsIgnoreCase("jerk"));
+            if (isInvalidWord){
+            		console.printf("That word is not allowed. try again\n");
+            }
+        } while (isInvalidWord);
         String adverb = console.readLine("Almost there, Just one adverb:  ");
         String verb = console.readLine("Last one, Just one verb:  ");
 
